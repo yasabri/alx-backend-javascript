1c979eb2fdaf97f83696cfcf5311747d2a6b77b6
yasabri <yasabri04@gmail.com>
2024-08-06 12:18:38 -0700

    me

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}
+   * @param {Number} maxStudentsSize - number students
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
