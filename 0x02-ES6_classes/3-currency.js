4643d015d65ebb156bfca4c4a4b82c27840321ed
yasabri <yasabri04@gmail.com>
2024-08-06 12:46:26 -0700

    me

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  /**
+   * Creates full string representation
+   * @returns
+   */
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
